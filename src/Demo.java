650fb82d4b9058bdcb88e3fd0efbfd2129f1ce80
vikrantgit97 <114150687+vikrantgit97@users.noreply.github.com>
2022-12-06 13:25:57 +0530

    Update Demo.java
    editing in remote repository (github )make pull request to make changes in local repository
    commiting directly to master branch

--- src/Demo.java
+++ src/Demo.java
@@ -3,5 +3,6 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("added new demo class ");
+		System.out.println("editing in remote repository (github )make pull request to make changes in local repository ");
 	}
 }
